beaef6376b0523ebb9603890b8069b6673c9a092
Your Name <you@example.com>
2023-09-30 17:15:00 -0700

    template for making ajax call

---
+++ src/script.js
@@ -0,0 +1,17 @@
+//import ajax
+
+// 2 const var's
+const clientId = "24c1ee3495f44ae7ae83eccae3955650"
+const code = undefined; 
+
+
+//use ajax to connect func's from py file 
+$.ajax({
+
+})
+//checks to see if there is a code
+//if there is no code, user is redirected to authorization page
+if (!code) {
+    //add func to redirect the user, call from py file
+}
+
