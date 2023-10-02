364adede5d1268e4d5737cfc121237316448bb27
Your Name <you@example.com>
2023-10-02 14:52:03 -0700

    jquery packages

--- src/script.js
+++ src/script.js
@@ -5,13 +5,15 @@ const clientId = "24c1ee3495f44ae7ae83eccae3955650"
 const code = undefined; 
 
 
-//use ajax to connect func's from py file 
-$.ajax({
 
-})
 //checks to see if there is a code
 //if there is no code, user is redirected to authorization page
 if (!code) {
     //add func to redirect the user, call from py file
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile)
 }
 
